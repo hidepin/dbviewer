d6b27c1ae0e8ea153d4ae863e7f80fa7e5814862
Hidetoshi Imai <hidepin@gmail.com>
2019-03-23 10:19:00 +0900

    Add disk_usage template.

--- dbviewer.py
+++ dbviewer.py
@@ -14,6 +14,8 @@ parser.add_argument('folder', type=str, nargs='?', default='download',
 parser.add_argument('--token', type=str, default=TOKEN,
                     help='Access token '
                     '(see https://www.dropbox.com/developers/apps)')
+parser.add_argument('--size', '-s', action='store_true',
+                    help='Display sum of file size.')
 
 def main():
     args = parser.parse_args()
@@ -26,7 +28,25 @@ def main():
 
     dbx = dropbox.Dropbox(args.token)
 
-    list_folder(dbx, folder, '')
+    if args.size:
+        disk_usage(dbx, folder, '');
+    else:
+        list_folder(dbx, folder, '')
+
+def disk_usage(dbx, folder, subfolder):
+    path = '/%s/%s' % (folder, subfolder.replace(os.path.sep, '/'))
+    while '//' in path:
+        path = path.replace('//', '/')
+    path = path.rstrip('/')
+    try:
+        if stopwatch('disk_usage'):
+            res = dbx.files_list_folder(path, recursive=True)
+    except dropbox.exceptions.ApiError as err:
+        print('Folder listing failed for', path, '-- assumed empty:', err)
+        return {}
+    else:
+        size = 0
+        disk_usage_recursive(dbx, res)
 
 def list_folder(dbx, folder, subfolder):
     path = '/%s/%s' % (folder, subfolder.replace(os.path.sep, '/'))
@@ -40,7 +60,6 @@ def list_folder(dbx, folder, subfolder):
         print('Folder listing failed for', path, '-- assumed empty:', err)
         return {}
     else:
-        rv = {}
         for entry in res.entries:
             print(entry.name)
 
@@ -51,6 +70,14 @@ def stopwatch(message):
     finally:
         t1 = time.time()
         print('Total elapsed time for %s: %.3f' % (message, t1 - t0))
+
+def disk_usage_recursive(dbx, res):
+    for entry in res.entries:
+        print(entry.name)
+
+    if (res.has_more):
+        res_continue = dbx.files_list_folder_continue(res.cursor)
+        disk_usage_recursive(dbx, res_continue)
 
 if __name__ == '__main__':
     main()
